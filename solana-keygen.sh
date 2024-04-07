8e0328fcb83dd7b1275c2de4956cdf1beb384da6
Peter Ipung <58955410+LucyFireID@users.noreply.github.com>
2024-04-08 06:06:28 +0700

    Update solana-keygen.sh

--- solana-keygen.sh
+++ solana-keygen.sh
@@ -6,7 +6,7 @@ jumlah_wallet=7
 # Loop untuk membuat wallet
 for ((i=1; i<=jumlah_wallet; i++))
 do
-    nama_wallet="uka$i"
+    nama_wallet="baru$i"
     solana-keygen new --outfile "$nama_wallet.json"
     echo "Wallet $nama_wallet telah dibuat"
 done
